770f7714e5a34bf70efb4377cbe860895f7345da
Chirag3112 <chirag123192@gmail.com>
2019-06-14 23:45:25 -0400

    addPlan controller

---
+++ src/main/java/com/asa/CRP/controller/AddPlanController.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.asa.CRP.controller;
+
+import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.asa.CRP.commons.PropertiesFileLoader;
+import com.asa.CRP.commons.Utils;
+import com.asa.CRP.model.Plan;
+import com.asa.CRP.service.AdminService;
+
+/**
+ * @author Chirag Vora
+ * @version 1.0
+ *
+ */
+
+@Controller
+public class AddPlanController {
+
+	private Logger logger = Logger.getLogger(AddPlanController.class);
+	
+	@Autowired
+	private AdminService adminService;
+	
+	/**
+	 * Properties file loader
+	 */
+	protected PropertiesFileLoader propertiesLoader = PropertiesFileLoader.getInstance();
+
+	/**
+	 * Property
+	 */
+	protected Properties property = propertiesLoader.getMiscProperties();
+
+	@RequestMapping(value = "/addplan", method = RequestMethod.GET)
+	public String addPlan(ModelMap model) {
+		return "addPlans";
+	}
+	
+	@RequestMapping(value = "/addPlanConfirmation", method = RequestMethod.GET)
+	public String createPlan(@RequestParam Map<String,String> reqPar, HttpSession httpSession, ModelMap model) {
+		if(Utils.validateCRSession(httpSession)){
+			logger.info("I am writing in add plans");
+			Plan plan = new Plan();
+			plan.setPlanName(reqPar.get("planName"));
+			plan.setPlanType(reqPar.get("planType"));
+			plan.setPlanDuration(reqPar.get("duration"));
+			plan.setPlanAmount(Integer.valueOf(reqPar.get("amount")));
+			adminService.addPlans(plan);
+			return "admin";
+		} else {
+			return "unauthorized";
+		}
+	}
+}
