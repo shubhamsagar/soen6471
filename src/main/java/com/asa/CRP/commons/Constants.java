60192d15073be2fbeb7f2a207d1097e5969da6a9
Shubham Sagar <shubham.sagar7696@gmail.com>
2019-06-18 11:32:28 -0400

    updated Constants

--- src/main/java/com/asa/CRP/commons/Constants.java
+++ src/main/java/com/asa/CRP/commons/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 	
 	final String RESULTS_FOUND="Results found:";
 	final String RESULTS_NOT_FOUND="No user with the given details was found!";
-	static final String SESSION_LOGGED_IN_FLAG = "crLoggedIn";
+	static final String SESSION_LOGGED_IN_FLAG ="crLoggedIn";
 
 }
--- src/main/java/com/asa/CRP/commons/DisplayMessages.java
+++ src/main/java/com/asa/CRP/commons/DisplayMessages.java
@@ -1,8 +1,6 @@
 package com.asa.CRP.commons;
 
 public class DisplayMessages {
-	
 	final String RESULTS_FOUND="Results found:";
 	final String RESULTS_NOT_FOUND="No user with the given details was found!";
-
 }
