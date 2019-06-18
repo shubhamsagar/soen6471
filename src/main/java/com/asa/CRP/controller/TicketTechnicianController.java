0fa70f95e0bf028be232ea901afc697cd6cac6d4
Shubham Sagar <shubham.sagar7696@gmail.com>
2019-06-18 11:50:51 -0400

    change in method

--- src/main/java/com/asa/CRP/controller/TicketTechnicianController.java
+++ src/main/java/com/asa/CRP/controller/TicketTechnicianController.java
@@ -1,7 +1,6 @@
 package com.asa.CRP.controller;
 
 import java.util.List;
-import java.util.Properties;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +8,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.asa.CRP.commons.PropertiesFileLoader;
-import com.asa.CRP.model.Plan;
 import com.asa.CRP.model.Ticket;
 import com.asa.CRP.service.TicketService;
 
