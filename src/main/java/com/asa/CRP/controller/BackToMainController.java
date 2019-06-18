9fa4d459b8a9a690d586a44daf3c46fca7d81c1a
Shubham Sagar <shubham.sagar7696@gmail.com>
2019-06-18 11:43:53 -0400

    update back to main

--- src/main/java/com/asa/CRP/controller/BackToMainController.java
+++ src/main/java/com/asa/CRP/controller/BackToMainController.java
@@ -1,28 +1,20 @@
 package com.asa.CRP.controller;
 
-import java.util.Map;
 import java.util.Properties;
-
 import javax.servlet.http.HttpSession;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.asa.CRP.commons.CRRoles;
 import com.asa.CRP.commons.PropertiesFileLoader;
 import com.asa.CRP.commons.Utils;
-import com.asa.CRP.model.Customer;
 import com.asa.CRP.model.CustomerRepresentative;
 import com.asa.CRP.service.CustomerRepresentativeService;
-import com.asa.CRP.service.CustomerService;
-import com.asa.CRP.service.PlanService;
+
 
 @Controller
 public class BackToMainController {
